87dc519fff33f86a1a45403cfcfc2caac5f07aaf
sarah <sarahakouri@gmail.com>
2024-06-26 14:20:59 +0200

    Envoi projet github

---
+++ main/views.py
@@ -0,0 +1,8 @@
+
+from django.shortcuts import render
+from .models import Article
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'main/article_list.html', {'articles': articles, 'redirect_to': request.path})
+
